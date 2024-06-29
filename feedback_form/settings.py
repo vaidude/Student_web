0e06faeb98e670d85384d93f7d912da091c69601
vaidude <vaivaideesh33@gmail.com>
2024-06-29 14:20:20 +0530

    allowed host setup

--- feedback_form/settings.py
+++ feedback_form/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y=oat3uou*a@^ibr&a^%dcfvz7l0ah97tg3&^raykioymv4s(i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
